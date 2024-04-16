1474fb256b0db8364f269d3e94a7aaa46661c58b
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-04-16 16:51:27 +0200

    step 4: Adding the Angular Output Target

--- packages/stencil-library/stencil.config.ts
+++ packages/stencil-library/stencil.config.ts
@@ -1,4 +1,5 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget } from '@stencil/angular-output-target';
 
 export const config: Config = {
   namespace: 'stencil-library',
@@ -19,8 +20,14 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    angularOutputTarget({
+      componentCorePackage: 'stencil-library',
+      outputType: 'component',
+      directivesProxyFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile: '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
   ],
   testing: {
-    browserHeadless: "new",
+    browserHeadless: 'new',
   },
 };
